da396f521149ee161b6eee7cc5c333a52e7ad365
Douglas Francisco <douglasfrancardoso@gmail.com>
2020-07-29 17:47:09 -0300

    Fazendo e gerando token JWT Validando o token na requisição

--- app.js
+++ app.js
@@ -4,12 +4,29 @@ const app = express();
 const Cliente = require('./src/cliente')
 const Catalogo = require('./src/catalogo')
 const { db } = require('./src/database');
+const jwt = require('jsonwebtoken');
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
 app.use(bodyParser.json());
+
+function verifyJWT(req, res, next){
+    var token = req.headers['authorization'];
+    if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
+
+    token = token.split(" ")
+
+    if (token[0] !== "Bearer") return res.status(401).json({ auth: false, message: 'Incorrect token type.' });
+
+    jwt.verify(token[1],'2Y%uZsm/HzKG%4z-Vft,mZ+oh[I].of5', function(err, decoded) {
+        if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+
+        req.idcliente = decoded.idcliente;
+        next();
+    });
+}
 
 app.post("/cliente", (req, res) => {
     new Cliente(
@@ -26,12 +43,23 @@ app.post("/cliente", (req, res) => {
 })
 
 app.post("/login", (req, res) => {
+    Cliente.login(req.body.email, req.body.senha)
+        .then((success) => {
+            res.json(success)
+        })
+        .catch((error) => {
+            res.json({ mensagem: error.message, sucesso: false })
+        })
+})
 
-
-
-    res.json({
-        sucesso: cliente.save()
-    })
+app.post("/logout", verifyJWT, (req, res) => {
+    Cliente.logout()
+        .then((success) => {
+            res.json(success)
+        })
+        .catch((error) => {
+            res.json({ mensagem: error.message, sucesso: false })
+        })
 })
 
 app.listen(3000, () => {
--- src/cliente/index.js
+++ src/cliente/index.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcrypt');
 const { db } = require('../database');
 const { v4: uuidv4 } = require('uuid');
 const email = require("email-validator");
+var jwt = require('jsonwebtoken');
 
 const saltRounds = 10;
 
@@ -20,27 +21,74 @@ class Cliente {
                 reject({
                     message: "E-mail inválido"
                 })
-            } else {
-                db().run("INSERT INTO cliente(idcliente, nome, email, senha) values (?,?,?,?)", [
-                    uuidv4(), this.nome, this.email, this.hash
-                ], function (err) {
-                    if (err) {
-                        console.log(err.message);
-                        reject(err)
-                    }
 
-                    resolve(true)
-                });
+                return;
             }
+
+            db().run("INSERT INTO cliente(idcliente, nome, email, senha) values (?,?,?,?)", [
+                uuidv4(), this.nome, this.email, this.hash
+            ], function (err) {
+                if (err) {
+                    reject(err)
+                    return;
+                }
+
+                resolve(true)
+            });
         })
     }
 
     static login(email, senha) {
-        bcrypt.compareSync(senha, hash); // true
+        return new Promise((resolve, reject) => {
+            var sql = "select * from cliente where email = ?"
+            var params = [email]
+
+            db().get(sql, params, (err, row) => {
+                if (err) {
+                    reject(err)
+                    return;
+                }
+
+                if (!bcrypt.compareSync(senha, row.senha)) {
+                    reject({message: "Senha incorreta!"})
+                    return;
+                }
+
+                // cria o JWT
+                var token = jwt.sign(
+                    {
+                        "idcliente": row.idcliente,
+                        "nome": row.nome,
+                        "email": row.email
+                    },
+                    '2Y%uZsm/HzKG%4z-Vft,mZ+oh[I].of5',
+                    {
+                        expiresIn: 3600 // 1hr
+                    }
+                );
+
+                resolve({
+                    "access_token": token,
+                    "token_type": "Bearer",
+                    "expires_in": 3600,
+                    "data" : {
+                        "nome": row.nome,
+                        "email": row.email
+                    }
+                })
+            });
+        })
     }
 
     static logout() {
-
+        return new Promise((resolve, reject) => {
+            resolve({
+                "access_token": null,
+                "token_type": "Bearer",
+                "expires_in": 3600,
+                "data" : {}
+            })
+        })
     }
 }
 
